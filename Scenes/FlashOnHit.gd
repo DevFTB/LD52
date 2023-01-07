7166d322a4b8358a9d6aa19b35632cc2b9b6bf56
Arthur <thepandaparadox@gmail.com>
2023-01-07 17:19:27 +1100

    extend flash on hit to player characters

--- Scenes/FlashOnHit.gd
+++ Scenes/FlashOnHit.gd
@@ -14,3 +14,8 @@ func _process(delta):
 func _on_enemy_health_changed(new_health, difference, should_display):
 	play("EnemyFlashOnHit")
 	pass # Replace with function body.
+
+
+func _on_character_health_changed(new_health):
+	play("EnemyFlashOnHit")
+	pass # Replace with function body.
