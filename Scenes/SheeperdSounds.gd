a1df01adc125c2a0552e5499ab79cb1588997db8
Arthur <thepandaparadox@gmail.com>
2023-01-09 10:50:52 +1100

    sound pitch changes

--- Scenes/SheeperdSounds.gd
+++ Scenes/SheeperdSounds.gd
@@ -1,16 +1,19 @@
 extends Node2D
+var rng = RandomNumberGenerator.new()
+var pitchRandom : float = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+	
 
 func _on_the_sheepard_skill_used():
 	$SkillSound.play()
 
 func _on_the_sheepard_attack_used():
+	rng.randomize()
+	pitchRandom = rng.randf_range(0.5,1.5)
 	$AttackSound.play()
-
+	$AttackSound.set_pitch_scale(pitchRandom)
+	
 func _on_the_sheepard_death():
 	$DeathSound.play()
 
