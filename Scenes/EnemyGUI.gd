46625ee9acd7f4867b1c1ae95dd59477c15b497a
Ryan <ryanwp526@gmail.com>
2023-01-09 01:46:45 +1100

    Add healthbars for enemies not @ full health

--- Scenes/EnemyGUI.gd
+++ Scenes/EnemyGUI.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$HealthBar.visible = false
 	pass # Replace with function body.
 
 
@@ -14,4 +15,12 @@ func _on_health_changed(new_health, difference, should_display):
 	if should_display:
 		if difference < 0:
 			$DamageNumber.start_animation(difference)
+	
+	var max_health = get_parent().max_hp
+	if new_health == max_health:
+		$HealthBar.visible = false
+	else:
+		$HealthBar.visible = true
+		print("log", new_health, max_health)
+		$HealthBar.value = round((new_health/max_health) * 100)
 			
