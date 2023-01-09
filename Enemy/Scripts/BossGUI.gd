f231b41cfdfef8c394165e300424639d8a701027
Ryan <ryanwp526@gmail.com>
2023-01-09 18:12:15 +1100

    Health bars complete
    Pending beautification

--- Enemy/Scripts/BossGUI.gd
+++ Enemy/Scripts/BossGUI.gd
@@ -1,26 +1,26 @@
 extends Control
-#
-#@export var health_bar : NodePath
-#@export var health_percentage : NodePath
-#@export var boss_name : NodePath
-#
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	$CanvasLayer.get_node("Control").get_node("VBoxContainer").get_node("BossName").text = get_parent().enemy_name
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-#
-#func _on_health_changed(new_health, difference, should_display):
-#	if should_display:
-#		if difference < 0:
-#			$DamageNumber.start_animation(difference)
-#
-#	var max_health = get_parent().max_hp
-#	$CanvasLayer.get_node("Control").get_node("VBoxContainer").get_node("HealthBar").value = round((new_health/max_health) * 100)
-#	$CanvasLayer.get_node("Control").get_node("VBoxContainer").aget_node("HealthPercentage").text = str(round((new_health/max_health) * 100))
-#
-#
+
+@export var health_bar : NodePath
+@export var health_percentage : NodePath
+@export var boss_name : NodePath
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node(boss_name).text = get_parent().enemy_name
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_health_changed(new_health, difference, should_display):
+	if should_display:
+		if difference < 0:
+			$DamageNumber.start_animation(difference)
+
+	var max_health = get_parent().max_hp
+	get_node(health_bar).value = round((new_health/max_health) * 100)
+	get_node(health_percentage).text = str(round((new_health/max_health) * 100)) + "%"
+
+
